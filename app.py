09f509b5826d2ec423d213b009d09d90f95fe54e
alexanderbpowers <101031351+alexanderbpowers@users.noreply.github.com>
2022-09-07 18:03:46 +1000

    munging files cleaned

--- app.py
+++ app.py
@@ -1,33 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
 ## Dependencies and set up
 
 import numpy as np
 import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
-postgres_uri = ""
-engine = create_engine(postgres_uri)
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+# postgres_uri = ""
+# engine = create_engine(postgres_uri)
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
 
-north_table = Base.classes.north
-stations = Base.classes.stations
-
-
+# north_table = Base.classes.north
+# stations = Base.classes.stations
 
 ## Routes
 
 app = Flask(__name__)
 
+postgres_uri = 'postgresql://irkqykpauqvpzm:b6a2bf95436de0394e29cb2c7b916b56f18fe92c6a3f5ac85638d94c4a93b081@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d5tqri7nt2h2k0'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(postgres_uri, '')  # or "sqlite:///db.sqlite"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from .models import North
+from .models import Stations
+
+
+
+
 @app.route("/")
 def home():
-    print("Home")
-    return (
-        f"Available routes:<br/>"
-        f"/api/north<br/>"
-        f"/api/stations")
+    return render_template("index.html")
 
 
 @app.route("/api/north")
@@ -56,3 +81,32 @@ def merged():
 
 if __name__ == "__main__":
     app.run(debug=False)
+
+
+
+####
+@app.route("/api/pals")
+def pals():
+    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+    hover_text = [result[0] for result in results]
+    lat = [result[1] for result in results]
+    lon = [result[2] for result in results]
+
+    pet_data = [{
+        "type": "scattergeo",
+        "locationmode": "USA-states",
+        "lat": lat,
+        "lon": lon,
+        "text": hover_text,
+        "hoverinfo": "text",
+        "marker": {
+            "size": 15,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
+    }]
+
+    return jsonify(pet_data)
